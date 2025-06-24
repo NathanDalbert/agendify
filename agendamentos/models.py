7eda2b3786a65310b57b5fe6e4243ae11c8edc0a
NathanDalbert <dalbert.nathan@gmail.com>
2025-06-24 11:39:18 -0300

    feat: Define o modelo Agendamento e cria migration inicial

--- agendamentos/models.py
+++ agendamentos/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from core.models import BaseModel 
 
-# Create your models here.
+class Agendamento(BaseModel):
+    prestador = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='agendamentos'
+    )
+    data_horario = models.DateTimeField()
+    nome_cliente = models.CharField(max_length=255)
+    email_cliente = models.EmailField()
+    telefone_cliente = models.CharField(max_length=20, blank=True, null=True)
+    cancelado = models.BooleanField(default=False)
+
+    class Meta:
+       
+        unique_together = ('prestador', 'data_horario')
+        ordering = ['data_horario'] 
+
+    def __str__(self):
+        return f"{self.nome_cliente} com {self.prestador.username} em {self.data_horario.strftime('%d/%m/%Y %H:%M')}"
