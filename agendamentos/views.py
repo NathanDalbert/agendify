1df5cc76f239065ca3d0d6ff3c5ef11bd3048455
NathanDalbert <dalbert.nathan@gmail.com>
2025-06-24 11:44:45 -0300

    feat: Implementa a API de Agendamentos com ViewSet e rotas

--- agendamentos/views.py
+++ agendamentos/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Agendamento
+from .serializers import AgendamentoSerializer 
 
-# Create your views here.
+class AgendamentoViewSet(viewsets.ModelViewSet):
+    serializer_class = AgendamentoSerializer
+
+    def get_queryset(self):
+        """Filtra os agendamentos para retornar apenas os do usuário logado."""
+        user = self.request.user
+
+        return Agendamento.objects.filter(prestador=user, cancelado=False)
+
+    def perform_create(self, serializer):
+        """Associa o usuário logado como prestador ao criar um agendamento."""
+        serializer.save(prestador=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        """Muda a lógica de DELETE para um 'soft delete' (apenas cancela)."""
+        agendamento = self.get_object()
+        agendamento.cancelado = True
+        agendamento.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
